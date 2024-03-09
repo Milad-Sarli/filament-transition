71a5032338ae32c6a05e7b212bd8ef065401e1b6
milad <miladsarli@hotmail.com>
2024-03-10 01:02:03 +0330

    update new sections

--- resources/dist/filament-transition.js
+++ resources/dist/filament-transition.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   setInterval(() => {
     var sections = document.querySelectorAll('.fi-section');
     var tables = document.querySelectorAll('.fi-ta');
+    var stats = document.querySelectorAll('.fi-wi-stats-overview-stat');
     sections.forEach(function (section, index) {
       setTimeout(function () {
         section.classList.add('fade-in');
@@ -10,6 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
     tables.forEach(function (table, index) {
       setTimeout(function () {
         table.classList.add('fade-in');
+      },index  * 200)
+    })
+    stats.forEach(function (stat, index) {
+      setTimeout(function () {
+        stat.classList.add('fade-in');
       },index  * 200)
     })
   },200)
