7f7432d751a4f2de18f1836c326b96bf1f4f880c
milad <miladsarli@hotmail.com>
2024-03-10 00:52:32 +0330

    some bugs fixes

--- resources/dist/filament-transition.js
+++ resources/dist/filament-transition.js
@@ -1,14 +1,16 @@
-var sections = document.querySelectorAll('.fi-section');
-var tables = document.querySelectorAll('.fi-ta');
-setInterval(() => {
-  sections.forEach(function (section, index) {
-    setTimeout(function () {
-      section.classList.add('fade-in');
-    },index  * 200)
-  })
-  tables.forEach(function (table, index) {
-    setTimeout(function () {
-      table.classList.add('fade-in');
-    },index  * 200)
-  })
-},200)
+document.addEventListener('DOMContentLoaded', function () {
+  setInterval(() => {
+    var sections = document.querySelectorAll('.fi-section');
+    var tables = document.querySelectorAll('.fi-ta');
+    sections.forEach(function (section, index) {
+      setTimeout(function () {
+        section.classList.add('fade-in');
+      },index  * 200)
+    })
+    tables.forEach(function (table, index) {
+      setTimeout(function () {
+        table.classList.add('fade-in');
+      },index  * 200)
+    })
+  },200)
+})
